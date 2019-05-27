c854d36526921cc1b78630d881e07a58d743be0b
leyton <leyton01@protonmail.com>
2019-05-27 22:45:56 +0800

    test2

---
+++ selenium/test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/27 22:43
+# @Author  : leyton
+# @Site    : 
+# @File    : test2.py
+# @Software: PyCharm
+
+import os
+
+print(os.path.dirname(__file__))
